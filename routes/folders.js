7ef7744d6edd8102b0aed4a57d798f28eafeb5c0
Gabriel Salas <ggsalas@gmail.com>
2019-06-07 17:52:41 -0300

    [createAppFlow] - new folder flow (#1)

--- routes/folders.js
+++ routes/folders.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
 const googleDrive = require('../controllers/googleDrive')
 
-// Get one folder from Google Drive
+// Get one folder contects from Google Drive
 router.get('/:googleFolderId', async (req, res) => {
   try {
     const folder = await googleDrive.getFolder({ googleFolderId: req.params.googleFolderId })
 
-    res.status(200).json(folder)
+    res.status(200).send(folder)
   } catch (error) {
-    res.status(400).json(error)
+    res.status(404).json(error.message || 'Folder not found')
   }
 })
 
